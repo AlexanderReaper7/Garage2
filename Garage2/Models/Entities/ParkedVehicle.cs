592a0b09fe4786672b2b89f0640be94b98f513c7
Alexander Öberg <stativetroller7@gmail.com>
2023-11-17 14:57:29 +0100

    add userstories

--- Garage2/Models/Entities/ParkedVehicle.cs
+++ Garage2/Models/Entities/ParkedVehicle.cs
@@ -28,6 +28,7 @@ public class ParkedVehicle
     public DateTime ArrivalTime { get; set; }
     /// <summary>
     /// Where this vehicle is parked with the first int being index of whole parking slots and second the sub-slot.
+    /// If This value is 0, then the vehicle is not parked.
     /// If the vehicle takes atleast 1 whole slot then the second int is always 0.
     /// </summary>
     [Display(Name = "Parking Lot Nr")]
